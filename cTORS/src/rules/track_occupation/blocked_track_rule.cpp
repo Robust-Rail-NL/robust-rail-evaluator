45f457ddf45a80c5e12d5e507ba5fbdfe9bd8cd7
Léon Planken <l.r.planken@tudelft.nl>
2026-03-27 11:09:17 +0000

    Check that a move action doesn't reserve an already occupied track (resulting in a collission).

--- cTORS/src/rules/track_occupation/blocked_track_rule.cpp
+++ cTORS/src/rules/track_occupation/blocked_track_rule.cpp
@@ -6,14 +6,26 @@
  * 1. The Action uses a Track that is reserved in the current State.
  * @return A pair describing 1) whether the action is valid, and 2) if not, why
  * 
- * In the current version this business rule is no different from blocked_track_rule. 
- * If multi-move actions are introduced, this rule will check that the path is never blocked by other 
- * ShuntingUnits
+ * For multi-move actions, this rule checks that the path is never blocked by other 
+ * ShuntingUnits.
  */
 pair<bool, string> blocked_track_rule::IsValid(const State* state, const Action* action) const {
 	auto ress = action->GetReservedTracks();
 	for (auto res : ress) {
-		if (state->IsReserved(res)) return make_pair(false, "Track " + res->toString() + " is reserved.");
+		if (state->IsReserved(res)) {
+			return make_pair(false, "Track " + res->toString() + " is reserved.");
+		}
+	}
+
+	// Check that no tracks reserved by the action (except possibly the first and the last) are
+	// occupied.
+	// This works because saw moves are not a single multi-move action, but always have a walking
+	// action in between. So all tracks we run through are fully traversed by the ShuntingUnit.
+	for ( size_t i = 1; i + 1 < ress.size(); ++i ) {
+		auto res = ress.at(i);
+		if ( state->GetOccupations(res).size() > 0 ) {
+			return make_pair(false, "Track " + res->toString() + " is occupied.");
+		}
 	}
 	return make_pair(true, "");
 }

b3166c983dd4c84462efbbde503a37e9178d0b5d
Léon Planken <l.r.planken@tudelft.nl>
2026-06-08 16:00:24 +0200

    Take a step back

--- build.sh
+++ build.sh
@@ -3,8 +3,11 @@
 # This file is intended to be run from inside a Docker container build from
 # the included Dockerfile.
 
+eval "$(conda shell.bash hook)"
+
+conda init
 conda activate my-proto-env
-mkdir /workspace/robust-rail-generator/build
-cd /workspace/robust-rail-generator/build
+mkdir /workspace/robust-rail-evaluator/build
+cd /workspace/robust-rail-evaluator/build
 cmake .. -DCONDA_ENV="/opt/conda/envs/my_proto_env"
 cmake --build .

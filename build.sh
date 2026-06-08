7746175645c497557bbe6c5f8ac70879d5bc84cd
Léon Planken <l.r.planken@tudelft.nl>
2026-06-08 15:44:53 +0200

    Split up commands

--- build.sh
+++ build.sh
@@ -3,12 +3,8 @@
 # This file is intended to be run from inside a Docker container build from
 # the included Dockerfile.
 
-cd /workspace/robust-rail-evaluator
-conda env create -f env.yml
-conda init
-source ~/.bashrc
 conda activate my-proto-env
-mkdir build
-cd build
+mkdir /workspace/robust-rail-generator/build
+cd /workspace/robust-rail-generator/build
 cmake .. -DCONDA_ENV="/opt/conda/envs/my_proto_env"
 cmake --build .
